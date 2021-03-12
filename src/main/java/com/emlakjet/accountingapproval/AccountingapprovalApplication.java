2898d3094d3c9196b006b3cb66fbb20fce68b76b
Mami <gultekinmsg@gmail.com>
2021-03-12 19:42:23 +0300

    Initial Commit

---
+++ src/main/java/com/emlakjet/accountingapproval/AccountingapprovalApplication.java
@@ -0,0 +1,13 @@
+package com.emlakjet.accountingapproval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingapprovalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountingapprovalApplication.class, args);
+    }
+
+}
